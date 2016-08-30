2ce322c44634be833794efb05ea276d183d21a01
11494032 <11494032@qq.com>
2016-08-30 09:37:45 +0800

    listview

---
+++ app/src/main/java/com/example/moresmart_pc006/listview/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.moresmart_pc006.listview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static  final String TAG = "MainActivity";
+
+    private ListView lv = null;
+
+    private ArrayList<String> data;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        data = new ArrayList<String>();
+
+        for( int i = 0; i < 50; i++ )
+            data.add("我是数据：" + i);
+
+        lv =(ListView) findViewById(R.id.LV);
+        MyAdapter myAdapter = new MyAdapter();
+        lv.setAdapter( myAdapter );
+
+    }
+
+
+    private class MyAdapter extends BaseAdapter
+    {
+        @Override
+        public int getCount() {
+
+            Log.d(TAG, "getCount");
+            return data.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return data.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+
+            Log.d(TAG,"getView");
+            TextView textView = new TextView(MainActivity.this);
+
+            textView.setText((String)getItem(i));
+
+            return textView;
+
+        }
+    }
+
+}
